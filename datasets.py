1d10e0fa9ec26dd147038d59780c24a93059fee2
jinxinzhou <48702671+jinxinzhou@users.noreply.github.com>
2021-05-05 23:26:54 -0400

    Add files via upload

--- datasets.py
+++ datasets.py
@@ -6,18 +6,27 @@ import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
 
-def make_dataset(dataset_name, data_dir, batch_size=128, sample_size=None):
+def make_dataset(dataset_name, data_dir, batch_size=128, sample_size=None, SOTA=False):
 
     if dataset_name == 'cifar10':
         print('Dataset: CIFAR10.')
-        trainset = CIFAR10(root=data_dir, train=True, download=True, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        if SOTA:
+            trainset = CIFAR10(root=data_dir, train=True, download=True, transform=transforms.Compose([
+                transforms.RandomCrop(size=32, padding=4),
+                transforms.RandomHorizontalFlip(p=0.5),
+
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
+                ]))
+        else:
+            trainset = CIFAR10(root=data_dir, train=True, download=True, transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
             ]))
 
         testset = CIFAR10(root=data_dir, train=False, download=True, transform=transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
             ]))
         num_classes = 10
     elif dataset_name == 'mnist':
